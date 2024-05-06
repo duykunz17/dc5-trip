6498b640fca441c0cd9be63f5a51fad5a956b7a9
Duy Trinh <trinhducduy10798@gmail.com>
2024-05-06 18:19:22 +0700

    Add gallary page, map page and fix ui

--- src/components/Utilities.jsx
+++ src/components/Utilities.jsx
@@ -1,39 +1,63 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { ImageListItem, ImageList } from '@material-ui/core';
+
+const itemData = [
+    {
+        img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+        title: 'Breakfast',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+        title: 'Burger',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+        title: 'Camera',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+        title: 'Coffee',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+        title: 'Hats',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+        title: 'Honey',
+    },
+
+];
 
 class Utilities extends Component {
     render() {
         return (
             <div className="travel_variation_area">
-                <div className="container">
-                <div className="row">
-                    <div className="col-lg-4 col-md-6">
-                    <div className="single_travel text-center">
-                        <div className="icon">
-                        <img src="images/1.svg" alt="" />
+                <div className="container" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', overflow: 'hidden', flexDirection: 'column' }}>
+                    <div className="row justify-content-center">
+                        <div className="col-lg-6">
+                            <div className="section_title text-center">
+                                <h3>HÌNH ẢNH NỔI BẬT</h3>
+                            </div>
                         </div>
-                        <h3>Comfortable Journey</h3>
-                        <p>A wonderful serenity has taken to the possession of my entire soul.</p>
                     </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6">
-                    <div className="single_travel text-center">
-                        <div className="icon">
-                        <img src="images/2.svg" alt="" />
+
+                    <ImageList rowHeight={250} className={{ width: 500, height: 450 }} cols={3}>
+                        {itemData.map((item) => (
+                            <ImageListItem key={item.img} cols={item.cols || 1}>
+                                <img src={item.img} alt={item.title} />
+                            </ImageListItem>
+                        ))}
+                    </ImageList>
+
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="more_place_btn text-center" style={{marginTop: "62px"}}>
+                                <Link to='/gallary' className="boxed-btn4" >Xem thêm</Link>
+                            </div>
                         </div>
-                        <h3>Luxuries Hotel</h3>
-                        <p>A wonderful serenity has taken to the possession of my entire soul.</p>
                     </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6">
-                    <div className="single_travel text-center">
-                        <div className="icon">
-                        <img src="images/3.svg" alt="" />
-                        </div>
-                        <h3>Travel Guide</h3>
-                        <p>A wonderful serenity has taken to the possession of my entire soul.</p>
-                    </div>
-                    </div>
-                </div>
                 </div>
             </div>
         );
