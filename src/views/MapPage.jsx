0ba206eea31b03cd28dd09ce0a50dc29a475dfff
nhtu <nhtu@tma.com.vn>
2024-05-07 11:24:14 +0700

    + Rotate map mobile

--- src/views/MapPage.jsx
+++ src/views/MapPage.jsx
@@ -3,15 +3,52 @@ import Header from '../components/Home/Header';
 import Footer from '../components/Home/Footer';
 
 class MapPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isMobile: false,
+        };
+    }
+    componentDidMount() {
+        this.checkIsMobile();
+        window.addEventListener('resize', this.checkIsMobile);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.checkIsMobile);
+    }
+
+    checkIsMobile = () => {
+        const isMobile = window.innerWidth <= 900; // Adjust the breakpoint as needed
+        this.setState({ isMobile });
+    };
+
     render() {
+        const { isMobile } = this.state;
+        const containerStyle = {
+            maxWidth: '100vw', // Ensure the container takes the full width of the viewport
+            transform: 'rotate(90deg)'
+        };
+
+        const imageStyle = {
+            height: '100vw', // Let the height adjust to maintain aspect ratio
+        };
+
         return (
             <div>
                 <Header />
+                {
+                    isMobile ? <div style={containerStyle}>
+                        <img style={imageStyle}  src="../images/map.jpg" alt="Landscape Image" />
+                    </div> :
+                        <img className="img-fluid" src="../images/map.jpg" alt="Landscape Image" />
+                }
+                {
+                    !isMobile && <Footer />
+                }
 
-                <img className="img-fluid" src="../images/map.jpg"></img>
+            </div >
 
-                <Footer />
-            </div>
         );
     }
 }
